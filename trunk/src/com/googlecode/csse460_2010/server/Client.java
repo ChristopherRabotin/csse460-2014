139a5e9545bb5c016ddab4e764e68c283cffccde
christopher.rabotin <christopher.rabotin@9059bfca-1c25-11df-b275-e7300661a1c9>
2010-02-18 09:51:30 +0000

    Started implementing a protocol. But I still have to figure out how to efficiently manage that. I think putting all the commands in the XML file works the best, but then I have to figure out what to do with the commands which need a second argument. Simply create an array of commands in the command class? I'll sleep on it.
    Protocol idea:
    go + direction (up,down,left,right)
    show + something (players,help,daemons,points,attacks)
    help
    daemoninfo + daemonName
    playerinfo + playerName (TODO: add an ID!)
    serverinfo (to show number of playing, uptime, etc.)

---
+++ trunk/src/com/googlecode/csse460_2010/server/Client.java
@@ -0,0 +1,69 @@
+package com.googlecode.csse460_2010.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Stack;
+
+/**
+ * The Client class is in the MCServer class file as it is only called by the
+ * MCServer class.
+ * 
+ * @author Christopher Rabotin
+ * 
+ */
+public class Client extends Thread {
+	private Socket socket = null;
+	private PrintWriter out;
+	private BufferedReader in;
+	private String inputLn, outputLn;
+	private Player me;
+	private Stack<String> msgQ;
+
+	public Client(Socket socket) {
+		super("StirlingZygote");
+		this.socket = socket;
+		msgQ = new Stack<String>();
+	}
+
+	public void run() {
+		try {
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket
+					.getInputStream()));
+
+			me = new Player("test", 1000);
+			Stirling.addPlayer(this); // we "register" our selves to the game engine
+
+			while ((inputLn = in.readLine()) != null) {
+				// if there are messages pending to be sent (e.g. Multicast
+				// msg), we send them now!
+				while (msgQ.size() > 0) {
+					out.println(msgQ.pop());
+				}
+
+			}
+			killClient();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void queueMsg(String msg) {
+		msgQ.push(msg);
+	}
+	
+	public void killClient(){
+		Stirling.rmPlayer(this);
+		out.close();
+		try {
+			in.close();
+			socket.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
--- trunk/src/com/googlecode/csse460_2010/server/MCServer.java
+++ trunk/src/com/googlecode/csse460_2010/server/MCServer.java
@@ -6,61 +6,26 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Stack;
 
 public class MCServer {
+	private static boolean listening = true;
 	public static void startServer(int port) throws IOException {
-        ServerSocket serverSocket = null;
-        boolean listening = true;
-
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 4444.");
-            System.exit(-1);
-        }
-
-        while (listening)
-	    new Client(serverSocket.accept()).start();
-
-        serverSocket.close();
-    }
-}
-/**
- * The Client class is in the MCServer class file as it is only called by the MCServer class.
- * @author Christopher Rabotin
- *
- */
-class Client extends Thread{
-	private Socket socket = null;
-	private enum PlayerStates {idle, fighting, eog};
-	private PlayerStates state=PlayerStates.idle;
-	private PrintWriter out;
-	private BufferedReader in;
-	private String inputLn, outputLn;
-
-	public Client(Socket socket) {
-		super("StirlingZygote");
-		this.socket = socket;
-	}
-
-	public void run() {
+		ServerSocket serverSocket = null;
 
 		try {
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket
-					.getInputStream()));
-
-			Stirling.addPlayer(new Player("test", 1000));
-			
-			while ((inputLn = in.readLine()) != null){
-				
-			}
-			out.close();
-			in.close();
-			socket.close();
-
+			serverSocket = new ServerSocket(port);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("Could not listen on port: 4444.");
+			System.exit(-1);
 		}
-	}	
+
+		while (listening)
+			new Client(serverSocket.accept()).start();
+
+		serverSocket.close();
+	}
+	public static void stopServer(){
+		listening = false;
+	}
 }
