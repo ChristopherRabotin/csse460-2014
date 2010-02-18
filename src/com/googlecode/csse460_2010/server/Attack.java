a06129590d94b833d569ff8cc3ff643be70a3df7
Chris <ChristopherRabotin@users.noreply.github.com>
2010-02-18 02:39:22 +0000

    Added a couple classes

---
+++ src/com/googlecode/csse460_2010/server/Attack.java
@@ -0,0 +1,11 @@
+package com.googlecode.csse460_2010.server;
+
+/**
+ * Attack contains all the information about any attack retrieved from the XML
+ * file.
+ * 
+ * @author Christopher Rabotin
+ */
+public class Attack {
+
+}
---
+++ src/com/googlecode/csse460_2010/server/Daemon.java
@@ -0,0 +1,10 @@
+package com.googlecode.csse460_2010.server;
+
+/**
+ * Daemon defines what a Daemon is.
+ * 
+ * @author Christopher Rabotin
+ */
+public class Daemon {
+
+}
