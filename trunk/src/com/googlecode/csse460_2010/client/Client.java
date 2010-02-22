5c7a563735368714686cd68377b68309c90658e3
christopher.rabotin <christopher.rabotin@9059bfca-1c25-11df-b275-e7300661a1c9>
2010-02-22 09:56:13 +0000

    Just realized I had wrongly named a package.

---
+++ trunk/src/com/googlecode/csse460_2010/client/Client.java
@@ -0,0 +1,82 @@
+package com.googlecode.csse460_2010.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Client {
+	static private int port, timeout;
+	static private String host, name, room, inputLn;
+	static private InetAddress addr;
+	static private SocketAddress sockaddr;
+	static private Socket skt;
+	static private boolean processRaw = false;
+	static private BufferedReader readFromSkt;
+	static private PrintWriter writeToSkt;
+
+	public static void main(String[] args) {
+		/*
+		 * We start by reading the XML file to communicate with the server.
+		 */
+		try {
+			XMLParser.loadNParse("clientConf.xml");
+		} catch (Throwable e) {
+			System.err.println("Error while loading the XML file!");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		port = XMLParser.getCnxPort();
+		timeout = XMLParser.getCnxTimeout();
+		host = XMLParser.getCnxHost();
+		/*
+		 * Then we print the welcome message to the user.
+		 */
+		System.out.println(XMLParser.getClientMsgs().get("welcome"));
+		name = System.in.toString();
+		System.out.println(XMLParser.parseMsg(XMLParser.getClientMsgs().get("welcome"),XMLParser.class));
+		try {
+			addr = InetAddress.getByName(host);
+			sockaddr = new InetSocketAddress(addr, port);
+
+			skt = new Socket();
+
+			/*
+			 *  This method will block no more than timeoutMs.
+			 *  If the timeout occurs, SocketTimeoutException is thrown.
+			 */
+			skt.connect(sockaddr, timeout);
+			readFromSkt = new BufferedReader(new InputStreamReader(skt
+					.getInputStream()));
+			writeToSkt = new PrintWriter(skt.getOutputStream(), true);
+
+			writeToSkt.println(name);
+			while ((inputLn = readFromSkt.readLine()) != null) {
+				process(inputLn);
+			}
+			
+			readFromSkt.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public static void process(String str) {
+		if (processRaw) { // if we are processing a raw input then we simply
+			// output it, no questions asked
+			System.out.println(str);
+		} else if (str.startsWith("endraw")) {
+			processRaw = false; // we don't print out anything as endraw is on
+			// it own line
+		} else if (!str.startsWith("raw")) {
+			processRaw = true; // we don't print out anything as raw is on it
+			// own line
+		} else {
+			// then we look up the meaning in the XMLParser class.
+		}
+	}
+}
---
+++ trunk/src/com/googlecode/csse460_2010/client/XMLParser.java
@@ -0,0 +1,154 @@
+package com.googlecode.csse460_2010.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class XMLParser {
+	private static int cnxPort, cnxTimeout;
+	private static String cnxHost;
+	private static Document xmlDoc;
+	private static Element cnxRoot, cmdsRoot, gameMsgRoot;
+	private static HashMap<String, Command> cmds = new HashMap<String, Command>();
+	private static HashMap<String, String> serverMsgs = new HashMap<String, String>(),
+			clientMsgs = new HashMap<String, String>();
+
+	public static void loadNParse(String xmlF) throws JDOMException,
+			IOException {
+		xmlDoc = (new SAXBuilder()).build(new File(xmlF));
+		cnxRoot = xmlDoc.getRootElement().getChild("Cnx");
+		cmdsRoot = xmlDoc.getRootElement().getChild("Cmds");
+		gameMsgRoot = xmlDoc.getRootElement().getChild("GameMsgs");
+		parseCnx();
+		parseCmds();
+		parseGameMsg();
+		xmlDoc.getRootElement().getChildTextNormalize("WelcomeMsg");
+	}
+
+	private static void parseCnx() {
+		cnxHost = cnxRoot.getAttributeValue("host");
+		cnxPort = Integer.parseInt(cnxRoot.getAttributeValue("port"));
+		cnxTimeout = Integer.parseInt(cnxRoot.getAttributeValue("timeout"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void parseCmds() {
+		List cmdsXML = cmdsRoot.getChildren("cmds");
+		Iterator cmdsXMLIt = cmdsXML.iterator();
+		Element current, argsXML;
+		Command cmd;
+		while (cmdsXMLIt.hasNext()) {
+			current = (Element) cmdsXMLIt.next();
+			argsXML = current.getChild("args");
+			cmd = new Command(current.getAttributeValue("client"), current
+					.getAttributeValue("server"), current.getTextNormalize(),
+					argsXML.getAttributeValue("serverValid"), argsXML
+							.getAttributeValue("clientValid"));
+			cmds.put(current.getAttributeValue("client"), cmd);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void parseGameMsg() {
+		/*
+		 * We start by getting all the messages which concern the game it-self.
+		 */
+		List msgsXML = gameMsgRoot.getChildren("ClientMsg");
+		Iterator msgsXMLIt = msgsXML.iterator();
+		Element current;
+		while (msgsXMLIt.hasNext()) {
+			current = (Element) msgsXMLIt.next();
+			clientMsgs.put(current.getAttributeValue("id"), current
+					.getTextNormalize());
+		}
+		/*
+		 * Then we get the messages which are related to the information sent by
+		 * the server.
+		 */
+		msgsXML = gameMsgRoot.getChildren("message");
+		msgsXMLIt = msgsXML.iterator();
+		while (msgsXMLIt.hasNext()) {
+			current = (Element) msgsXMLIt.next();
+			serverMsgs.put(current.getAttributeValue("for"), current
+					.getTextNormalize());
+		}
+	}
+
+	/**
+	 * Parses the serverWelcomeMsg to replace all the occurrences of variables
+	 * with their actual value. The variables are words starting with a $ sign.
+	 * 
+	 * @param msg
+	 *            the message to be parsed by the function
+	 * @param cls
+	 *            the class which contains the fields of the text variables
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static String parseMsg(String msg, Class cls) {
+		String[] splt, splt1;
+		Field fd;
+		while (msg.contains("$")) {
+			/*
+			 * We start by splitting the message by all the occurrences of
+			 * variables.
+			 */
+			splt = msg.split("\\$");
+			for (int i = 1; i < splt.length; i += 2) {
+				/*
+				 * then for every even value (the one where the first word
+				 * corresponds to the field), we extract the first word (which
+				 * is why we split by non-alpha numerical character).
+				 */
+				splt1 = splt[i].split("\\W");
+				try {
+					/*
+					 * Finally we get the field in the given class, convert that
+					 * to string and replace the original string.
+					 */
+					fd = cls.getDeclaredField(splt1[0]);
+					fd.setAccessible(true);
+					msg = msg.replace("$" + splt1[0], String.valueOf(fd
+							.get(null)));
+				} catch (Throwable e) {
+					System.out.println("[" + splt1[0] + "]");
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+		}
+		return msg;
+	}
+
+	public static String getCnxHost() {
+		return cnxHost;
+	}
+
+	public static int getCnxTimeout() {
+		return cnxTimeout;
+	}
+
+	public static int getCnxPort() {
+		return cnxPort;
+	}
+
+	public static HashMap<String, Command> getCmds() {
+		return cmds;
+	}
+
+	public static HashMap<String, String> getServerMsgs() {
+		return serverMsgs;
+	}
+
+	public static HashMap<String, String> getClientMsgs() {
+		return clientMsgs;
+	}
+}
